b5da3aa96ff263e202612f5c1e63bfa617fd3e10
Prarthna <prarthna76@gmail.com>
2020-09-14 18:55:25 -0400

    not sure

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,14 @@
+import csv
+import pandas as pf
 
+r = pf.read_csv(python.csv)
+r.head
+
+print r.describe()
+BudgetData = r['Budget Data'].unioque()
+r.Data = 'Budget Data'
+for data in BudgetData:
+outfilesData = Date
+printDate = outfilesData + ".csv"
+print printDate
+r[r['Budget Data'] == Date].to_csv(printDate, index=False)
