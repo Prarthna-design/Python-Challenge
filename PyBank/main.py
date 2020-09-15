95331eb436bc4628255c4b0d8a53af4133840dc3
Prarthna <prarthna76@gmail.com>
2020-09-15 01:28:58 -0400

    print Financial Analysis

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,13 +6,17 @@ output_csv = os.path.join("Resources", "budget_data.csv")
 
 print('Financial Analysis')
 
+#open file, read and print header
 
+with open(output_csv) as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=',')
+    csvheader = next(csv_file)
+    print(f'Header: {csvheader}')
 
-
-
-
-
-
+    # total number of months included in the dataset
+    next(csvreader)
+    months = len(list(csvreader))
+    print(f'Total Months: {months}')
 
 
 
